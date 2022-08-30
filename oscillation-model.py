148cbfa520eccf30491f50ed6a4eb0a8630a107b
Korra Pickell <ronanpickell@gmail.com>
2022-08-30 14:12:44 -0500

    + upsample block

--- oscillation-model.py
+++ oscillation-model.py
@@ -1,18 +1,39 @@
 
 import tensorflow as tf
-from keras.layers import Input, Conv2D, BatchNormalization, LeakyReLU
+from keras.layers import Input, Conv2D, Dropout, Conv2DTranspose, BatchNormalization, LeakyReLU, ReLU
 from keras.activations import relu, leaky_relu
 
 TARGET_SIZE = (512,512,3)
 
 def downsample(filters, size, apply_batchnorm=False):
     initializer = tf.random_normal_initializer(0., 0.02)
-    x = Conv2D(filters,size,strides=2,padding='same',kernel_initializer=initializer, use_bias=False)
+    x = Conv2D(filters,
+                size,
+                strides=2,
+                padding='same',
+                kernel_initializer=initializer,
+                use_bias=False)
+
     if apply_batchnorm:
         x = BatchNormalization()(x)
     x = LeakyReLU()(x)
     return x
 
+
+def upsample(filters, size, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
+    x = Conv2DTranspose(filters,
+                        size,
+                        strides=2,
+                        padding='same',
+                        kernel_initializer=initializer,
+                        use_bias=False)
+
+    x = BatchNormalization()(x)
+    if apply_dropout:
+        x = Dropout(0.5)(x)
+    x = ReLU()(x)
+    return x
 
 def get_model_branch0():
     input0 = Input(shape=(512,512,3,))
