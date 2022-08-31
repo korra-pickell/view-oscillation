38108089eccb4a744c2cb08147a26ca37f983b9c
Korra Pickell <ronanpickell@gmail.com>
2022-08-31 18:03:50 -0500

    +load_data()

--- oscillation-model.py
+++ oscillation-model.py
@@ -3,9 +3,11 @@ import tensorflow as tf
 from keras.layers import Input, Dense, Conv2D, Reshape, Dropout, Conv2DTranspose, BatchNormalization, LeakyReLU, ReLU, Flatten, Concatenate
 from keras.activations import relu, leaky_relu
 
-TARGET_SIZE = (512,512,3)
+TARGET_SHAPE = (128,128,3,)
+SHUFFLE_BUFFER = 500
+BATCH_SIZE = 128
 
-data = r'E:\DATA\View Oscillation 2\128.npz'
+data_path = r'E:\DATA\View Oscillation 2\128.npz'
 
 
 def generator(path):
@@ -15,7 +17,33 @@ def generator(path):
             yield {"input_1":x_0, "input_2": x_1}, y_0
 
 
+def get_dataset():
+    gen = generator(data_path)
+    dataset = tf.data.Dataset.from_generator(gen)
+    dataset.shuffle(SHUFFLE_BUFFER)
 
+    train_dataset = dataset.take(4200)
+    test_dataset = dataset.skip(4200)
+
+    train_dataset = train_dataset.batch(BATCH_SIZE)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+    return train_dataset,test_dataset
+
+
+def load_data():
+    with np.load(data_path) as data:
+        x0,x1,y0 = data['x0'],data['x1'],data['y0']
+    full_dataset = tf.data.Dataset.from_tensor_slices(([x0,x1], y0))
+
+    full_dataset = full_dataset.shuffle(1000)
+    
+    train_dataset = full_dataset.take(4200)
+    test_dataset = full_dataset.skip(4200)
+
+    train_dataset = train_dataset.batch(BATCH_SIZE)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+    
+    return train_dataset, test_dataset
 
 
 def downsample(filters, size, apply_batchnorm=True):
@@ -54,7 +82,7 @@ def upsample(filters, size, apply_dropout=False):
 
 
 def get_model_full():
-    input0 = Input(shape=(512,512,3,))
+    input0 = Input(shape=TARGET_SHAPE)
     input1 = Input(shape=(1,))
 
     down_stack = [downsample(64,4,apply_batchnorm=False),
@@ -62,17 +90,17 @@ def get_model_full():
                     downsample(256,4),
                     downsample(512,4),
                     downsample(512,4),
-                    downsample(512,4),
-                    downsample(512,4),
                     downsample(512,4)]
+                    #downsample(512,4),
+                    #downsample(512,4)]
 
     up_stack = [upsample(64,4, apply_dropout=True),
                     upsample(128,4, apply_dropout=True),
                     upsample(256,4, apply_dropout=True),
                     upsample(512,4),
-                    upsample(512,4),
-                    upsample(512,4),
                     upsample(512,4)]
+                    #upsample(512,4),
+                    #upsample(512,4)]
 
     x = input0
     
@@ -123,8 +151,13 @@ def save_graph(model):
                             dpi=64)
     print('Graph Saved')
 
-model = get_model_full()
-model.build(input_shape=(512,512,3,1))
-save_graph(model)
+
+
+if __name__ == '__main__':
     
+    model = get_model_full()
+    save_graph(model)
+    model.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError(), metrics=['mean_squared_error'])
+    train_dataset,test_dataset = load_data()
+    model.fit(generator(data_path),epochs=4)
 
