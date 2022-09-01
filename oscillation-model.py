7b872032cebf02bd8301d458788e47c84a7f35c2
Korra Pickell <ronanpickell@gmail.com>
2022-08-31 19:29:48 -0500

    main model converted back to concatenated branches

--- oscillation-model.py
+++ oscillation-model.py
@@ -3,18 +3,27 @@ import tensorflow as tf
 from keras.layers import Input, Dense, Conv2D, Reshape, Dropout, Conv2DTranspose, BatchNormalization, LeakyReLU, ReLU, Flatten, Concatenate
 from keras.activations import relu, leaky_relu
 
+
 TARGET_SHAPE = (128,128,3,)
 SHUFFLE_BUFFER = 500
-BATCH_SIZE = 128
+BATCH_SIZE = 2
+EPOCHS = 3
 
 data_path = r'E:\DATA\View Oscillation 2\128.npz'
 
 
 def generator(path):
     with np.load(path) as data:
-        x0,x1,y0 = data['x0'],data['x1'],data['y0']
-        for x_0,x_1,y_0 in zip(x0,x1,y0):
-            yield {"input_1":x_0, "input_2": x_1}, y_0
+        x0,y0 = data['x0'],data['y0']
+        x0 = np.expand_dims(x0,axis=0)
+        y0 = np.expand_dims(y0,axis=0)
+        #print(len(x0),len(x1),len(y0))
+        #s=input('...')
+        for x_0,y_0 in zip(x0,y0):
+            #print(x_0.shape,x_1.shape,y_0.shape)
+            #print(x_1)
+            #s=input('...')
+            yield x_0, y_0
 
 
 def get_dataset():
@@ -30,10 +39,12 @@ def get_dataset():
     return train_dataset,test_dataset
 
 
+
+'''
 def load_data():
     with np.load(data_path) as data:
         x0,x1,y0 = data['x0'],data['x1'],data['y0']
-    full_dataset = tf.data.Dataset.from_tensor_slices(([x0,x1], y0))
+    #full_dataset = tf.data.Dataset.from_tensor_slices({"input_1":x0, "input_2": x1}, "label":y0)
 
     full_dataset = full_dataset.shuffle(1000)
     
@@ -44,7 +55,7 @@ def load_data():
     test_dataset = test_dataset.batch(BATCH_SIZE)
     
     return train_dataset, test_dataset
-
+'''
 
 def downsample(filters, size, apply_batchnorm=True):
     initializer = tf.random_normal_initializer(0., 0.02)
@@ -81,51 +92,58 @@ def upsample(filters, size, apply_dropout=False):
     return result
 
 
-def get_model_full():
-    input0 = Input(shape=TARGET_SHAPE)
-    input1 = Input(shape=(1,))
+def get_branch0():
+    input0 = Input(shape=(128,128,3,))
 
-    down_stack = [downsample(64,4,apply_batchnorm=False),
-                    downsample(128,4),
-                    downsample(256,4),
-                    downsample(512,4),
-                    downsample(512,4),
-                    downsample(512,4)]
+    down_stack = [downsample(32,4,apply_batchnorm=False),
+                    downsample(32,4),
+                    downsample(32,4),
+                    downsample(32,4),
+                    downsample(32,4),
+                    downsample(32,4)]
                     #downsample(512,4),
                     #downsample(512,4)]
 
-    up_stack = [upsample(64,4, apply_dropout=True),
-                    upsample(128,4, apply_dropout=True),
-                    upsample(256,4, apply_dropout=True),
-                    upsample(512,4),
-                    upsample(512,4)]
-                    #upsample(512,4),
-                    #upsample(512,4)]
-
     x = input0
-    
-    #skips = []
 
     for down in down_stack:
         x = down(x)
-        #skips.append(x)
-
-    #x = Concatenate()([x,input1])
-
+    
     x = Flatten()(x)
-    x = Concatenate()([x,input1])
     x = Dense(2048)(x)
-    x = Reshape(target_shape=(2,2,512))(x)
 
-    #skips = reversed(skips[:-1])
+    model = tf.keras.models.Model(input0,x)
+    return model
 
+def get_branch1():
+    input1 = Input(shape=(1,))
+    x = input1
+    x = Dense(1)(x)
+    model = tf.keras.models.Model(input1,x)
+    return model
+
+
+def get_model_full():
+
+    branch0 = get_branch0()
+    branch1 = get_branch1()
+    combined_branches = Concatenate()([branch0.output,branch1.output])
+    x = Dense(2049)(combined_branches)
+    x = Dense(2048)(x)
+    x = Reshape((2,2,512,))(x)
+    
+    
+    up_stack = [upsample(32,4, apply_dropout=True),
+                    upsample(32,4, apply_dropout=True),
+                    upsample(32,4, apply_dropout=True),
+                    upsample(32,4),
+                    upsample(32,4)]
+                    #upsample(512,4),
+                    #upsample(512,4)]
+    
+    
     for up in up_stack:
         x = up(x)
-        #x = Concatenate()([x,skip])
-
-    #for up, skip in zip(up_stack,skips):
-        #x = up(x)
-        #x = Concatenate()([x,skip])
 
     initializer = tf.random_normal_initializer(0., 0.02)
 
@@ -138,7 +156,7 @@ def get_model_full():
 
     
 
-    return tf.keras.Model(inputs=[input0,input1],outputs=x)
+    return tf.keras.Model(inputs=[branch0.input,branch1.input],outputs=x)
 
 
 def save_graph(model):
@@ -158,6 +176,7 @@ if __name__ == '__main__':
     model = get_model_full()
     save_graph(model)
     model.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError(), metrics=['mean_squared_error'])
-    train_dataset,test_dataset = load_data()
-    model.fit(generator(data_path),epochs=4)
+    #train_dataset,test_dataset = load_data()
+    #model.fit(train_dataset, validation_data = test_dataset, epochs=EPOCHS)
+    #model.fit(generator(data_path),epochs=4)
 
