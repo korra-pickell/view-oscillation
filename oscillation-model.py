db18cff06218b3de436b87c878ff3b673d5f3f65
Korra Pickell <ronanpickell@gmail.com>
2022-09-01 10:15:45 -0500

    - load_data()

--- oscillation-model.py
+++ oscillation-model.py
@@ -14,16 +14,17 @@ data_path = r'E:\DATA\View Oscillation 2\128.npz'
 
 def generator(path):
     with np.load(path) as data:
-        x0,y0 = data['x0'],data['y0']
+        x0,x1,y0 = data['x0'],data['x1'],data['y0']
         x0 = np.expand_dims(x0,axis=0)
+        #x1 = np.expand_dims(x1,axis=0)
         y0 = np.expand_dims(y0,axis=0)
         #print(len(x0),len(x1),len(y0))
         #s=input('...')
-        for x_0,y_0 in zip(x0,y0):
+        for x_0,x_1,y_0 in zip(x0,x1,y0):
             #print(x_0.shape,x_1.shape,y_0.shape)
             #print(x_1)
             #s=input('...')
-            yield x_0, y_0
+            yield {'input_1':x_0, 'input_2':x_1}, y_0
 
 
 def get_dataset():
@@ -39,23 +40,6 @@ def get_dataset():
     return train_dataset,test_dataset
 
 
-
-'''
-def load_data():
-    with np.load(data_path) as data:
-        x0,x1,y0 = data['x0'],data['x1'],data['y0']
-    #full_dataset = tf.data.Dataset.from_tensor_slices({"input_1":x0, "input_2": x1}, "label":y0)
-
-    full_dataset = full_dataset.shuffle(1000)
-    
-    train_dataset = full_dataset.take(4200)
-    test_dataset = full_dataset.skip(4200)
-
-    train_dataset = train_dataset.batch(BATCH_SIZE)
-    test_dataset = test_dataset.batch(BATCH_SIZE)
-    
-    return train_dataset, test_dataset
-'''
 
 def downsample(filters, size, apply_batchnorm=True):
     initializer = tf.random_normal_initializer(0., 0.02)
@@ -110,15 +94,14 @@ def get_branch0():
         x = down(x)
     
     x = Flatten()(x)
-    x = Dense(2048)(x)
+    x = Dense(2048,activation='sigmoid')(x)
 
     model = tf.keras.models.Model(input0,x)
     return model
 
 def get_branch1():
-    input1 = Input(shape=(1,))
-    x = input1
-    x = Dense(1)(x)
+    input1 = Input(shape=(None,1))
+    x = Dense(1,activation='sigmoid')(input1)
     model = tf.keras.models.Model(input1,x)
     return model
 
@@ -128,8 +111,8 @@ def get_model_full():
     branch0 = get_branch0()
     branch1 = get_branch1()
     combined_branches = Concatenate()([branch0.output,branch1.output])
-    x = Dense(2049)(combined_branches)
-    x = Dense(2048)(x)
+    x = Dense(2049,'relu')(combined_branches)
+    x = Dense(2048,'sigmoid')(x)
     x = Reshape((2,2,512,))(x)
     
     
@@ -176,7 +159,7 @@ if __name__ == '__main__':
     model = get_model_full()
     save_graph(model)
     model.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError(), metrics=['mean_squared_error'])
-    #train_dataset,test_dataset = load_data()
+    #train_dataset,test_dataset = get_dataset()
     #model.fit(train_dataset, validation_data = test_dataset, epochs=EPOCHS)
-    #model.fit(generator(data_path),epochs=4)
+    model.fit(generator(data_path),epochs=4)
 
