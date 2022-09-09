6701fa33b89329709128e10f6868dce63ef66b57
Korra Pickell <ronanpickell@gmail.com>
2022-09-09 15:59:26 -0500

    data sorted by degree

--- data-handling.py
+++ data-handling.py
@@ -11,28 +11,45 @@ num_unique_scenes = 100
 def get_img(path):
     img = cv2.resize(cv2.imread(path),target_size)
     return (np.array(img)/127.5)-1
+'''
+def to_npz():
+    bar = Bar(' PROCESSING ', max = num_unique_scenes, suffix='%(percent)d%%')
+    example_index = 5
+    for n in range(example_index+1,num_unique_scenes):
+        d_imgs = []
+        f_index = 0
+        
+        for d in range(0,46):
+            d_imgs.append(get_img(os.path.join(data,str(d),str(n).zfill(4)+'.jpg')))
+        for i,img in enumerate(d_imgs):
+            x0 = d_imgs[0]
+            y0 = img
+            if f_index < 46:
+                os.mkdir(os.path.join(data2,str(f_index)))
+            f_index+= 1
+            
+            np.savez(os.path.join(data2,str(i),str(example_index).zfill(5)+'.npz'),
+                x0 = np.array(x0),
+                y0 = np.array(y0))
+            example_index += 1
+        #s = input('....')
+        bar.next()
+    bar.finish()'''
 
 
 def to_npz():
     bar = Bar(' PROCESSING ', max = num_unique_scenes, suffix='%(percent)d%%')
     example_index = 5
-    for n in range(6,num_unique_scenes):
-        d_imgs = []
-        for d in range(0,46):
-            d_imgs.append(get_img(os.path.join(data,str(d),str(n).zfill(4)+'.jpg')))
-        for i,img in enumerate(d_imgs):
-            x0 = d_imgs[0]
-            x1 = (float(i)/23)-1
-            y0 = img
-            np.savez(os.path.join(data2,str(example_index).zfill(5)+'.npz'),
-                x0 = np.array(x0), 
-                x1 = np.array(x1),
-                y0 = np.array(y0))
-            example_index += 1
-        bar.next()
-    bar.finish()
-        
-
-
+    init_folders = True
+    for d in range(0,46):
+        if init_folders:
+            os.mkdir(os.path.join(data2,str(d)))
+        for n in range(example_index+1,num_unique_scenes):
+            x = get_img(os.path.join(data,str(0),str(n).zfill(4)+'.jpg'))
+            y = get_img(os.path.join(data,str(d),str(n).zfill(4)+'.jpg'))
+            np.savez(os.path.join(data2,str(d),str(example_index).zfill(5)+'.npz'),
+                x = np.array(x),
+                y = np.array(y))
+        example_index += 1
 
 to_npz()
